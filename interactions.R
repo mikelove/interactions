35c4c494941f0d37e66b6d2f80218a853e2da26a
mikelove <michaelisaiahlove@gmail.com>
2019-05-23 14:30:31 -0400

    adding comments

--- interactions.R
+++ interactions.R
@@ -1,38 +1,55 @@
 # simulation of high dimensional case
 # desire: factorial design, interaction term
 
-n <- 40 # in four groups
-p <- 1000
-pi0 <- .95
+n <- 40 # samples, in four groups
+p <- 1000 # number of features, say genes
+pi0 <- .95 # proportion of null genes
 
+# suppose a 2x2 design
+# condition: control or treated
+# group: A or B
 condition <- factor(rep(rep(c("ctr","trt"),each=n/4),2))
 group <- factor(rep(c("A","B"),each=n/2))
 
+# suppose there is a shift across the two groups which is not of interest
 grp.eff <- 1
+
+# we are interested in differences in the condition effect across group
 cond.grp.interx.eff <- 2
 
+# some continuous data:
+# in classic genomics style, rows are features, columns are samples
 x <- matrix(rnorm(n*p),ncol=n)
+# add the group shift
 x[,group=="B"] <- x[,group=="B"] + grp.eff
+# add the interaction for (1-pi0) of the genes
 idx <- condition=="trt" & group=="B"
 x[1:(p-pi0*p),idx] <- x[1:(p-pi0*p),idx] + cond.grp.interx.eff
 
+# plot the data
 cols <- colorRampPalette(c("white","darkblue"))(99)
 image2 <- function(x) image(t(x[nrow(x):1,]),col=cols)
-
 image2(x)
 
-table(condition,group)
+# the sample groups
+table(condition, group)
 
+# define a function for later
 shuf <- function(l) sample(which(l),sum(l))
+# need this package for row-wise Wilcoxon,
+# install with:
+# install.packages("BiocManager")
+# BiocManager::install("siggenes")
 library(siggenes)
 
-nreps <- 20
-
+# function to compute row-wise wilcoxon on matrix 'z'
+# 'cl' is a binary vector (0/1) for the classes
 computeWilcoxon <- function(z, cl) {
   zzz <- capture.output({w <- rowWilcoxon(z, cl)})
   w
 }
 
+# a function to compute a proposed test statistic for the interaction
 computeTestStat <- function(x, npseudo, null=FALSE) {
   xa <- x[,group=="A"]
   xb <- x[,group=="B"]
